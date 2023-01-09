914d0c91a4d8d66f65a7aec21b13256842338621
Charles Obasi <charles@trycycledata.com>
2023-01-08 20:02:15 -0500

    Finish server

--- server/src/app.ts
+++ server/src/app.ts
@@ -16,6 +16,35 @@ const openai = new OpenAIApi(configuration);
 app.use(express.json({ limit: "500kb" }));
 app.use(express.urlencoded({ limit: "500kb", extended: true }));
 app.use(cors());
+
+app.get("/", async (req: Request, res: Response) => {
+  res.status(200).send({
+    message: "Hello",
+  });
+});
+
+app.post("/", async (req: Request, res: Response) => {
+  try {
+    const propmpt = req.body.prompt;
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${propmpt}`,
+      temperature: 0.7,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
+});
+
 try {
   app.listen(port, (): void => {
     console.log(`Server running on port ${port}`);
